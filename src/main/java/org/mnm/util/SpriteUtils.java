a733293ec704141390042f2c74d5841b15cafbe2
calvi <calvin@brianandkathi.com>
2024-02-01 19:07:56 -0500

    Updated player reference image.
    Added SpriteUtils class.

---
+++ src/main/java/org/mnm/util/SpriteUtils.java
@@ -0,0 +1,10 @@
+package org.mnm.util;
+
+public final class SpriteUtils {
+
+    public static final int FACING_DOWN = 0;
+    public static final int FACING_LEFT = 1;
+    public static final int FACING_UP = 2;
+    public static final int FACING_RIGHT = 3;
+
+}
